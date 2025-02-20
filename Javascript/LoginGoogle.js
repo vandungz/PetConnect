b293514353413eaff0580c59d115017d64747912
ngottuyetnhung4 <161111489+ngottuyetnhung4@users.noreply.github.com>
2025-02-20 10:50:57 +0700

    update login Google

--- Javascript/LoginGoogle.js
+++ Javascript/LoginGoogle.js
@@ -3,13 +3,13 @@ code + app secret -> accessToken
 lấy thông tin người dùng: avatar, name, mail
 ./auth/userinfo.email
 ./auth/userinfo.profile */
-const CLIENT_ID = "370070666207-5djtnr409mhau140tddj76hg5t5tbok8.apps.googleusercontent.com";
-const LINK_GET_TOKEN = `https://accounts.google.com/o/oauth2/v2/auth?scope=https://www.googleapis.com/auth/userinfo.email%20https://www.googleapis.com/auth/userinfo.profile&response_type=token&redirect_uri=https%3A//oauth2.example.com/code&client_id=${CLIENT_ID}`.replace(/\s+/g, '');
+const CLIENT_ID = "370070666207-doivth203a5ioi01brklkmh936nvrcbs.apps.googleusercontent.com";
+const LINK_GET_TOKEN = `https://accounts.google.com/o/oauth2/v2/auth?scope=https%3A//www.googleapis.com/auth/userinfo.email%20https%3A//www.googleapis.com/auth/userinfo.profile&response_type=token&redirect_uri=http://127.0.0.1:5500/PetConnect/Html/index.html&client_id=${CLIENT_ID}`.replace(/\s+/g, '');
 /* replace(/\s+/g, '') : xoá toàn bộ hàng và khoảng trắng thừa */
 
 /* gọi đăng nhập khi người dùng click vào button */
 document.addEventListener("DOMContentLoaded", () =>{
-    const sign_btn = document.querySelector(".btnGoogle");
+    const sign_btn = document.querySelector(".btn-loginGoogle");
     sign_btn.addEventListener("click", () =>{
         window.location.href = LINK_GET_TOKEN;
     });
@@ -26,8 +26,7 @@ const getInfoUser = async() =>{
     const accessToken = getToken();
     if(!accessToken) return;
     try{
-    const respone = await fetch(`https://www.googleapis.com/oauth2/v3/userinfo?access_token=${accessToken}`
-    );
+    const respone = await fetch(`https://www.googleapis.com/oauth2/v3/userinfo?access_token=${accessToken}`);
     const data = await respone.json();
     renderUI(data);
 } catch(error){
